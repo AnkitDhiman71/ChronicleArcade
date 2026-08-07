f63be15a22a31f325f1f8202558d823245c21ebd
ankitdhiman <ankitdhiman202005@gmail.com>
2026-08-07 21:55:20 +0530

    feat: create Mongoose user schema with authentication and role fields

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -36,7 +35,5 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
 const User = mongoose.model('User', userSchema);
-
 export default User;
